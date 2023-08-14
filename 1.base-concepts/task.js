faf20366598f3f694c1f1c5050f589761d67b7a9
SashaT29 <tusnetovaa@gmail.com>
2023-08-14 10:38:36 +0300

    Задание 1 и 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+  } else if(discriminant > 0) {
+    let firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let secondRoot = ((-b - Math.sqrt(discriminant)) / (2 * a));
+    arr.push(firstRoot, secondRoot);
+  }
+return arr;
+  }
+  solveEquation(1,2,1);
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let per = percent / 100 / 12; //преобразовали процентную ставку
+  let credit = amount - contribution; //рассчитали тело кредита
+  let pay = credit * (per + per/(Math.pow(1 + per, countMonths) - 1)); //ежемесечная оплата
+  let totalAmount = +(pay * countMonths).toFixed(2); //общая сумма оплаты
+
+  return totalAmount;
 }
