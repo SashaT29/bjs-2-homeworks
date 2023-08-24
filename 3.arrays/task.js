c35cce9c63322425ccac4e6535bcc7384c90cefd
SashaT29 <tusnetovaa@gmail.com>
2023-08-24 21:38:28 +0300

    Д\З по Массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
-}
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  return arr1.every((currentValue, index) => currentValue === arr2[index]);
+  }
+
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let usersGenders = users.filter((user) => user.gender === gender);
+  let usersAge = usersGenders.reduce((acc, user) => acc + user.age, 0); 
+  let avrAge = usersAge / (usersGenders.length || 1);
+  return avrAge;
 }
