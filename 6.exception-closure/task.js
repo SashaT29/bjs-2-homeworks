da208c22510b4dfdd0952717054f74e85f8a2b73
SashaT29 <tusnetovaa@gmail.com>
2023-09-03 20:51:09 +0300

    Обработка исключений и замыкание. Задача 1-2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,56 @@
-﻿
+﻿// Задача 1. Форматтер чисел
+
+function parseCount(value) {
+    if (Number.isNaN(Number.parseFloat(value))) {
+        throw new Error("Невалидное значение");
+    } else {
+        return parseFloat(Number(value));
+    }
+}
+
+
+function validateCount(value) {
+    try {
+       return parseCount(value); 
+    } catch(error) {  
+        return error;
+    }
+}
+
+
+//Задача 2. Треугольник
+
+class Triangle {
+    constructor (firstSide, secondSide, thirdSide) {
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+        this.thirdSide = thirdSide;  
+        
+        if ((firstSide + secondSide < thirdSide) || (firstSide + thirdSide < secondSide) || (secondSide + thirdSide < firstSide)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter() {
+        return this.firstSide + this.secondSide + this.thirdSide;
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        return Number((Math.sqrt(p*(p - this.firstSide)*(p - this.secondSide)*(p - this.thirdSide)).toFixed(3)));
+    }
+}
+
+   function getTriangle(firstSide, secondSide, thirdSide){
+    try {
+        return new Triangle(firstSide, secondSide, thirdSide)
+    }
+    catch(error){
+        return{
+            get perimeter() { return "Ошибка! Треугольник не существует"; },
+            get area() { return "Ошибка! Треугольник не существует";}
+            }
+        }
+
+    }
+
